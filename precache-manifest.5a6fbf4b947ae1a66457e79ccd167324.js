3b9b2a97ca7f75c68ebe7defb63c4a608ecc006a
Jesse <jessemckinney513@gmail.com>
2021-08-17 22:04:15 -0400

    Updates

---
+++ precache-manifest.5a6fbf4b947ae1a66457e79ccd167324.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "becfdd474973536b35eacee77d78ce8a",
+    "url": "/ShadowText/index.html"
+  },
+  {
+    "revision": "49a8ff7cd995fa75bc7f",
+    "url": "/ShadowText/static/css/main.3b9cae8e.chunk.css"
+  },
+  {
+    "revision": "bb18084104717ac0e556",
+    "url": "/ShadowText/static/js/2.e65f8c00.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ShadowText/static/js/2.e65f8c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a8ff7cd995fa75bc7f",
+    "url": "/ShadowText/static/js/main.4e324a43.chunk.js"
+  },
+  {
+    "revision": "e634d812fcfe8793c38e",
+    "url": "/ShadowText/static/js/runtime-main.6ae31dc9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ShadowText/static/media/logo.5d5d9eef.svg"
+  }
+]);
